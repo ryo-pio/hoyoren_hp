80885520cb454e1fc1c7ca9b42ad157d131979eb
ryo <ryosakoi21@icloud.com>
2019-07-12 14:06:26 +0900

    membersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'top' => 'top#index'
-  get 'member' => 'top#member'
   get 'activity' => 'top#activity'
   get 'performance' => 'top#performance'
   get 'infomation' => 'top#infomation'
+  get 'members' => 'members#index'
 end
