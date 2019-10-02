5d5749d6108efe55d7dd59b1c8be523548d6235e
ryo <ryosakoi21@icloud.com>
2019-10-02 22:27:15 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'hoyoren'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hoyoren'
+  config.fog_directory  = 'hoyoren-dev'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hoyoren-dev'
 end
