06c0e1dea99b2b7bf847636bf0332586bab6ef51
ryo <ryosakoi21@icloud.com>
2019-11-12 14:33:07 +0900

    basic認証解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
 
   def index
-  end
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'hoyoren' && password == '4351'
-    end
   end
 end
