4c6ba858e61b503a4b8d0c6f8133b267323b3901
ryo <ryosakoi21@icloud.com>
2019-11-12 14:15:15 +0900

    画面サイズによるcssの変更化

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,42 @@
 $(function() {
   //マウスを乗せたら発動
-  $('.main-header__list--font').hover(function() {
+  $('.main-header__list--home').hover(function() {
+    //マウスを乗せたら色が変わる
+    $(this).css('color', '#b33e5c');
+  //ここにはマウスを離したときの動作を記述
+  }, function() {
+    //色指定を空欄にすれば元の色に戻る
+    $(this).css('color', '');
+  });
+});
+
+$(function() {
+  //マウスを乗せたら発動
+  $('.main-header__list--member').hover(function() {
+    //マウスを乗せたら色が変わる
+    $(this).css('color', '#b33e5c');
+  //ここにはマウスを離したときの動作を記述
+  }, function() {
+    //色指定を空欄にすれば元の色に戻る
+    $(this).css('color', '');
+  });
+});
+
+$(function() {
+  //マウスを乗せたら発動
+  $('.main-header__list--performance').hover(function() {
+    //マウスを乗せたら色が変わる
+    $(this).css('color', '#b33e5c');
+  //ここにはマウスを離したときの動作を記述
+  }, function() {
+    //色指定を空欄にすれば元の色に戻る
+    $(this).css('color', '');
+  });
+});
+
+$(function() {
+  //マウスを乗せたら発動
+  $('.main-header__list--informations').hover(function() {
     //マウスを乗せたら色が変わる
     $(this).css('color', '#b33e5c');
   //ここにはマウスを離したときの動作を記述
